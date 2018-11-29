841abf5615719b2606c30d8f50514642378cfa44
Nicacioneto <nicacionetobsb@hotmail.com>
2018-11-28 22:06:46 -0200

    call python script with cron

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.5.3'
+
+gem 'twitter'
+gem 'daemons'
+gem 'pg'
+gem 'json'
+gem 'byebug'
+gem 'whenever', require: false
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,5 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, "$HOME/get_twitter_data/cron_log.log"
 
-# Example:
-#
-set :output, "/home/nicacio/get_twitter_data/cron_log.log"
-#
-every 1.minutes do
-  command "python3 $HOME/bitcoin_sentimental_analisys/candlestick_chart.py 13-10-dataframe.csv 2018-10-13-ts.txt"
-  # runner "MyModel.some_method"
-  # rake "some:great:rake:task"
+every 1.hour do
+  command "python3 $HOME/bitcoin_sentimental_analisys/chart_builder.py"
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
