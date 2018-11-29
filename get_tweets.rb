3168583c93a46030054ca0d512b8ab59ed9dc9d6
Nicacioneto <nicacionetobsb@hotmail.com>
2018-11-28 22:09:49 -0200

    update get_tweets

--- get_tweets.rb
+++ get_tweets.rb
@@ -3,10 +3,10 @@ require 'json'
 require 'byebug'
 
 client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
+  config.consumer_key        = "4X5DsljoD5l2Ahc0AtuDETtJ1"
+  config.consumer_secret     = "JSX6NP4BjCYUHM0pYhMLrmHD2Npy97tAmnG2Rghp1RK2y0zkVM"
+  config.access_token        = "960905033423474689-YrKoOvKq6krAgClFh3pbbFkUd0eglYB"
+  config.access_token_secret = "owtfhzRidnjkKSOIP1aGxWtIJzjzQeB6jBkuWtDiLvsP3"
 end
 
 
@@ -14,7 +14,7 @@ end
 topics = ["bitcoin", "btc", "satoshi"]
 client.filter(track: topics.join(",")) do |object|
   current_day = DateTime.now.day
-  file_name = "/home/nicacio_notebook/bitcoin_sentimental_analisys/" + current_day.to_s
+  file_name = "/home/nicacio_notebook/get_twitter_data/" + current_day.to_s
   tweets_stream = File.open(file_name, "a")
   if object.is_a?(Twitter::Tweet)
     hash = object.to_h
